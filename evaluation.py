68fa977f83b9642ea8caa85740a6941504f08ee5
Oleksii Chepur <chepuralex1991@gmail.com>
2023-02-13 19:06:36 +0000

    git init

---
+++ evaluation.py
@@ -0,0 +1,14 @@
+﻿def evaluate (x,y,choice):
+    result_1 = None
+    if choice == 1:
+        result_1 = x + y
+
+    elif choice == 2:
+        result_1 = x - y
+
+    elif choice == 3:
+        result_1 = x * y
+
+    elif choice == 4:
+        result_1 = x / y
+    return result_1
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-﻿def add(x, y):
+﻿from Calculator.evaluation  import evaluate
+
+def add(x, y):
     return x + y
 
 
@@ -12,4 +14,16 @@ def multiply(x, y):
 
 def divide(x, y):
     return x / y
-    
+
+
+x = input('Enter first number: ')
+y = input('Enter second number: ')
+print("""Select operation: 
+1. Add
+2. Substract
+3. Multiply
+4. Divide""")
+choice = int(input('Choose the option:'))
+
+evaluate(x,y,choice)
+print(evaluate(x,y,choice))
